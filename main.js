c94bd6dc663ab59996daa615ecef7d22d232942f
ErnaniDeMagalhaes <69161232+ErnaniDeMagalhaes@users.noreply.github.com>
2020-08-03 13:34:22 -0300

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-const startButton = document.getElementById('start');
-const helloWorld = document.getElementById('hello-world');
-
-startButton.addEventListener('click', function(){
-    helloWorld.classList.toggle('hello');
-});
